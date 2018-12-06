4acc91b860d9ef353bcbe997ca99c39f83d8fb43
raghu <nraghu366@gmail.com>
2018-12-06 15:37:03 +0530

    alpha fix

--- flints.py
+++ flints.py
@@ -48,13 +48,13 @@ app.layout=htm.Div(
             ],className="row"),
 
         htm.Div([
-            htm.Div([dcc.Graph(id='barGraph')], className='five columns'),
-            htm.Div(className='two columns'),
+            htm.Div([dcc.Graph(id='barGraph')], className='six columns'),
+            htm.Div(className='three columns'),
             htm.Div([
                 htm.H3('Share Price: ' +str(tbl['Price'])),
                 htm.H3('Exchange: ' +str(tbl['Exchange'])),
                 htm.H3('100 Shares: ' +str(100*tbl['UnitPrice']))
-                ],className='five columns')
+                ],className='three columns')
         ],className="row")
     ],className='ten columns offset-by-one')
 )
