7ddf608de7e737996082c1829f671939cf64c8a9
raghu <nraghu366@gmail.com>
2018-12-06 12:25:28 +0530

    changes

---
+++ tcalc.py
@@ -0,0 +1,11 @@
+from adsLib import *
+
+def getNAQticker(tkr,currency=False,rnum=5):
+	tkrcost=round(yfi.get_live_price(tkr),rnum)
+	tkrData=odict([('Ticker',tkr),('Price',tkrcost),('UnitPrice',tkrcost)])
+	if(currency):
+		ccr=ccrates()
+		xrate=round(ccr.get_rate('USD',currency),rnum)
+		tkrData['Exchange']=xrate
+		tkrData['UnitPrice']=round(xrate*tkrcost,rnum)
+	return tkrData
