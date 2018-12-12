329d7d6288e272eb6aa00dee1527c0100c61db18
raghu <nraghu366@gmail.com>
2018-12-12 22:16:32 +0530

    r1-beta

--- adsLib.py
+++ adsLib.py
@@ -1,9 +1,10 @@
 import os
 from collections import OrderedDict as odict
+from datetime import datetime as dtm,timedelta
 from copy import deepcopy
 import numpy as n
 from pandas import DataFrame as pdf,read_sql
-import pandas_datareader as pdr
+from pandas_datareader import data as pdr
 from yahoo_fin import stock_info as yfi
 from forex_python.converter import CurrencyRates as ccrates
 from pymongo import MongoClient as mcx
@@ -12,3 +13,5 @@ import dash
 import dash_core_components as dcc
 import dash_html_components as htm
 import dash_table as dtl
+from dash.dependencies import Input, Output
+from plotly import graph_objs as go
--- tcalc.py
+++ tcalc.py
@@ -1,11 +1,31 @@
 from adsLib import *
 
-def getNAQticker(tkr,currency=False,rnum=5):
-	tkrcost=round(yfi.get_live_price(tkr),rnum)
-	tkrData=odict([('Ticker',tkr),('Price',tkrcost),('UnitPrice',tkrcost)])
-	if(currency):
-		ccr=ccrates()
-		xrate=round(ccr.get_rate('USD',currency),rnum)
-		tkrData['Exchange']=xrate
-		tkrData['UnitPrice']=round(xrate*tkrcost,rnum)
+ccr=ccrates()
+
+def popForex():
+	flist=list(ccr.get_rates('USD',dtm.utcnow()).keys())
+	flist.append('USD')
+	return flist
+
+def getNAQticker(tkr,rnum=5):
+	if(tkr==None):
+		tkrData=odict([('Ticker',tkr),('Price',0)])
+	else:
+		tkrcost=round(yfi.get_live_price(tkr),rnum)
+		tkrData=odict([('Ticker',tkr),('Price',tkrcost)])
 	return tkrData
+
+xlist=popForex()
+
+def getXchSet(currency,ndays=188,rnum=3):
+	fullSet=odict()
+	if(currency in xlist):
+		today=dtm.utcnow().date()
+		i=0
+		while(i<ndays):
+			day=today-timedelta(i)
+			fullSet[day]=round(ccr.get_rate('USD',currency,day),rnum)
+			i+=1
+	else:
+		fullSet=False
+	return fullSet
