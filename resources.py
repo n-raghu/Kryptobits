9d991bb0a8c14a95cafa0a2ecaa342de0810b109
Raghu Neerukonda <Raghu.Neerukonda@inspiredelearning.com>
2019-10-30 17:33:28 +0530

    integrate error tracking system

--- resources.py
+++ resources.py
@@ -1,5 +1,5 @@
 import random
-from uuid import uuid4 as UU4
+from uuid import uuid1 as UU1, uuid4 as UU4
 from datetime import datetime as dtm
 from time import perf_counter as tpc
 
@@ -23,10 +23,6 @@ urx = 'postgresql://' +cfg['datastore']['uid']+ ':' +cfg['datastore']['pwd']+ '@
 now = tpc()
 pub_key_store = []
 pvt_key_store = []
-KAFKA = cfg['kafka']['enable']
-master_key = cfg['key']['master_key']
-global_key = cfg['key']['global_key']
-key_size = int(cfg['key']['key_size'])
 app_key = 'rH_wkQVjM3ub6LOD1qGNA8fff12cIvljEDwWtKj-VNw='
 
 
@@ -73,38 +69,83 @@ def record_key_request(key_id, requester):
     return None
 
 
-event_session = dataSession(urx)
-pub_key_store_en = event_session.query(K).filter(K.active==True).with_entities(K.key_id, K.pub_key).all()
-event_session.close()
-event_session = dataSession(urx)
-pvt_key_store_en = event_session.query(K).filter(K.deprecated==False).with_entities(K.key_id, K.pvt_key).all()
-event_session = dataSession(urx)
-unlock_set = [Fernet(app_key.encode()), Fernet(global_key.encode())]
-mft = MFT(unlock_set)
+try:
+    KAFKA = cfg['kafka']['enable']
+    master_key = cfg['key']['master_key']
+    global_key = cfg['key']['global_key']
+    key_size = int(cfg['key']['key_size'])
+except Exception as err:
+    record_error(
+        urx,
+        e_class='heart',
+        e_rsrc='Import App Config',
+        e_msg=str(err)
+    )
 
-for _ in pvt_key_store_en:
-    _tup_ = {'key_id': _[0]}
-    _pvt_ = serialization.load_pem_private_key(
-            _[1],
-            password=mft.decrypt(master_key.encode()),
-            backend=default_backend()
+try:
+    event_session = dataSession(urx)
+    pub_key_store_en = event_session.query(K).filter(K.active==True).with_entities(K.key_id, K.pub_key).all()
+    event_session.close()
+    event_session = dataSession(urx)
+    pvt_key_store_en = event_session.query(K).filter(K.deprecated==False).with_entities(K.key_id, K.pvt_key).all()
+    event_session = dataSession(urx)
+except Exception as err:
+    record_error(
+        urx,
+        e_class='heart',
+        e_rsrc='Import Keys from Store',
+        e_msg=str(err)
+    )
+
+try:
+    unlock_set = [Fernet(app_key.encode()), Fernet(global_key.encode())]
+    mft = MFT(unlock_set)
+except Exception as err:
+    record_error(
+        urx,
+        e_class='heart',
+        e_rsrc='Build MFT Object',
+        e_msg=str(err)
+    )
+
+try:
+    for _ in pvt_key_store_en:
+        _tup_ = {'key_id': _[0]}
+        _pvt_ = serialization.load_pem_private_key(
+                _[1],
+                password=mft.decrypt(master_key.encode()),
+                backend=default_backend()
         )
-    _pem_ = _pvt_.private_bytes(
-        encoding=serialization.Encoding.PEM,
-        format=serialization.PrivateFormat.PKCS8,
-        encryption_algorithm=serialization.NoEncryption()
-    ).decode()
-    _tup_['pvt_key'] = _pem_
-    pvt_key_store.append(_tup_)
+        _pem_ = _pvt_.private_bytes(
+            encoding=serialization.Encoding.PEM,
+            format=serialization.PrivateFormat.PKCS8,
+            encryption_algorithm=serialization.NoEncryption()
+        ).decode()
+        _tup_['pvt_key'] = _pem_
+        pvt_key_store.append(_tup_)
+    pvt_key_store_en.clear()
+except Exception as err:
+    record_error(
+        urx,
+        e_class='heart',
+        e_rsrc='Decrypt Private Store',
+        e_msg=str(err)
+    )
 
-pvt_key_store_en.clear()
-
-for _ in pub_key_store_en:
-    _tup_ = {
-        'key_id'  : _[0],
-        'pub_key' : _[1].decode(),
+try:
+    for _ in pub_key_store_en:
+        _tup_ = {
+            'key_id': _[0],
+            'pub_key': _[1].decode(),
         }
-    pub_key_store.append(_tup_)
+        pub_key_store.append(_tup_)
+except Exception as err:
+    record_error(
+        urx,
+        e_class='heart',
+        e_rsrc='Decode Public Store',
+        e_msg=str(err)
+    )
 
 if KAFKA:
     P = Producer({'bootstrap.servers': cfg['kafka']['host']})
@@ -112,17 +153,33 @@ if KAFKA:
 
 class PubKey(Resource):
     def get(self):
-        return jsonify(random.choice(pub_key_store))
+        try:
+            return jsonify(random.choice(pub_key_store))
+        except Exception as err:
+            record_error(
+                urx,
+                e_class='pubkey',
+                e_rsrc='get-pubkey',
+                e_msg=str(err)
+            )
 
 
 class PvtKey(Resource):
     @jwt_required
     def get(self):
-        if not request.json:
-            return jsonify('Invalid')
-        else:
-            k_id = request.json.get('key_id', None)
-            key_json = next(i for i in pvt_key_store if i['key_id'] == k_id)
-            key_json['requester'] = get_jwt_identity()
-            record_key_request(key_json['key_id'], key_json['requester'])
-            return jsonify(key_json)
+        try:
+            if not request.json:
+                return jsonify('Invalid')
+            else:
+                k_id = request.json.get('key_id', None)
+                key_json = next(i for i in pvt_key_store if i['key_id'] == k_id)
+                key_json['requester'] = get_jwt_identity()
+                record_key_request(key_json['key_id'], key_json['requester'])
+                return jsonify(key_json)
+        except Exception as err:
+            record_error(
+                urx,
+                e_class='pvtkey',
+                e_rsrc='get-pvtkey',
+                e_msg=str(err)
+            )
