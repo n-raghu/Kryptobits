79b83302396c3ca8ab7d9c834194eead5c79d0e2
Raghu Neerukonda <Raghu.Neerukonda@inspiredelearning.com>
2019-10-30 12:55:07 +0530

    PEP standards for service file

--- service.py
+++ service.py
@@ -5,15 +5,15 @@ from flask_jwt_extended import JWTManager
 
 from resources import cfg, PubKey, PvtKey
 
-app=Flask(__name__)
+app = Flask(__name__)
 fnt_instance = Fernet(cfg['key']['global_key'].encode())
 app.config['JWT_SECRET_KEY'] = fnt_instance.decrypt(cfg['key']['jwt_key'].encode()).decode()
 fnt_instance = False
 jwt = JWTManager(app)
 api = Api(app)
 
-api.add_resource(PubKey,'/krs/v1/pubkey')
-api.add_resource(PvtKey,'/krs/v1/pvtkey')
+api.add_resource(PubKey, '/krs/v1/pubkey')
+api.add_resource(PvtKey, '/krs/v1/pvtkey')
 
 app.run(
         debug=cfg['app']['debug'],
